c737278e476c55fdfa0173c090a68cdcb35c55d2
Alejandro <alejandro.1701520969@ucaldas.edu.co>
2023-02-20 11:52:15 -0500

    test

--- src/proxy_cobol/proxy.py
+++
@@ -1,6 +0,0 @@
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body':
-        """010080[CCO] CAMARAS COMERCIO DECISOR                                     0101CCD010080[CCO] CCO VS WS                                                    0121MAN990080012.900422614.900422614.[PROD=68].                                     !*♥"""
-    }
